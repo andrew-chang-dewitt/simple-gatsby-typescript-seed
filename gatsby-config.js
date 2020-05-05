0f63fbdc36dcd983236cd367f4db1f48bad47def
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-05-05 10:37:02 -0400

    graphql typing configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,21 @@
 require('ts-node').register({ files: true })
 
 module.exports = {
-  plugins: [`gatsby-plugin-typescript`],
+  siteMetadata: {
+    title: 'Andrew Chang-DeWitt',
+  },
+  plugins: [
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-codegen',
+      options: {},
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
