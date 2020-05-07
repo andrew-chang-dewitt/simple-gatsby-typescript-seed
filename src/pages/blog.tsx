f8bcbc775fcb7b19b0a7113baeb3005c5ee9f4c0
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-05-07 11:38:42 -0400

    adding pages, these will be simple "components" simply used to handle any graphql processing before handing off rendering responsibility to a different component from src/components. This makes the actual rendering components more testable without having to mock graphql

---
+++ src/pages/blog.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { Layout } from '../components/Layout'
+import { BlogHome, Post } from '../components/BlogHome'
+
+interface Props {
+  // FIXME: find proper type for this
+  data: any
+}
+
+const Blog = (props: Props) => {
+  const posts = props.data.allMarkdownRemark.edges.map(
+    ({ node }: any): Post => {
+      return {
+        id: node.id,
+        title: node.frontmatter.title,
+        date: node.frontmatter.date,
+        slug: node.fields.slug,
+        excerpt: node.excerpt,
+      }
+    }
+  )
+  return (
+    <Layout pageTitle="Blog">
+      <BlogHome
+        postCount={props.data.allMarkdownRemark.totalCount}
+        posts={posts}
+      />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default Blog
