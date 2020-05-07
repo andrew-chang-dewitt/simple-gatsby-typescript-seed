131363a0c5111f5ebcd72d9ea59e7aef2891f0af
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-05-07 11:36:48 -0400

    markdown parsing & page creation config added

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,49 @@
 require('ts-node').register({ files: true })
+
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === 'MarkdownRemark') {
+    const fileNode = getNode(node.parent)
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: 'src',
+      trailingSlash: false,
+    })
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve('./src/templates/blog-post.tsx'),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
+}
