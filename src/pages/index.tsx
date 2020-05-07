94809e74124ca167f3e18c7d434c6756a3287bda
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-05-07 12:52:53 -0400

    removing testing for pages & added Layout Component to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import { Layout } from '../components/Layout'
+
 const world: string = 'world'
 
 export default function Home() {
-  return <h1>Hello {world}!</h1>
+  return (
+    <Layout pageTitle="Home">
+      <h1>Hello {world}!</h1>
+    </Layout>
+  )
 }
